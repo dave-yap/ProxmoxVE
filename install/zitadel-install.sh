fbb1a353406320f3570e456c0449fe7d76a59205
Dave Yap <github@dave.yap-family.com>
2025-02-07 19:59:41 +0800

    Update zitadel-install.sh

--- install/zitadel-install.sh
+++ install/zitadel-install.sh
@@ -57,7 +57,7 @@ case $ARCH in
     i686) ARCH="386";;
     i386) ARCH="386";;
 esac
-wget -q -c https://github.com/zitadel/zitadel/releases/download/$LATEST/zitadel-linux-$ARCH.tar.gz -O - | tar -xz --quiet &>/dev/null
+wget -q -c https://github.com/zitadel/zitadel/releases/download/$LATEST/zitadel-linux-$ARCH.tar.gz -O - | tar -xzf - &>/dev/null
 mv zitadel-linux-$ARCH/zitadel /usr/local/bin
 rm -rf zitadel-linux-$ARCH
 msg_ok "Installed Zitadel"
