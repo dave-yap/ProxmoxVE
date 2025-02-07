b2a7c9a8419472dadee1ed43b9345275e97e6dec
Dave Yap <github@dave.yap-family.com>
2025-02-07 19:23:49 +0800

    Update zitadel-install.sh

--- install/zitadel-install.sh
+++ install/zitadel-install.sh
@@ -135,14 +135,14 @@ systemctl enable -q zitadel.service
 msg_ok "Created Services"
 
 msg_info "Start up Zitadel initial setup"
-timeout --kill-after=5s 30s zitadel start-from-init --masterkeyFile /opt/zitadel/.masterkey --config /opt/zitadel/config.yaml &>/dev/null
+timeout --kill-after=5s 30s zitadel start-from-init --masterkeyFile /opt/zitadel/.masterkey --config /opt/zitadel/config.yaml > /dev/null
 useradd zitadel
 msg_ok "Zitadel initialized"
 
 msg_info "Set ExternalDomain to current IP and restart Zitadel"
 IP=$(ip a s dev eth0 | awk '/inet / {print $2}' | cut -d/ -f1)
 sed -i "0,/localhost/s/localhost/${IP}/" /opt/zitadel/config.yaml
-zitadel setup --masterkeyFile /opt/zitadel/.masterkey --config /opt/zitadel/config.yaml &>/dev/null
+zitadel setup --masterkeyFile /opt/zitadel/.masterkey --config /opt/zitadel/config.yaml > /dev/null
 systemctl restart -q zitadel.service
 msg_ok "Zitadel restarted with ExternalDomain set to current IP"
 
