9c3be2408bc4b990966d7d7aaaa5bf3db254673f
Dave Yap <github@dave.yap-family.com>
2025-02-26 01:30:00 +0800

    Update seafile-install.sh

--- install/seafile-install.sh
+++ install/seafile-install.sh
@@ -81,7 +81,7 @@ sudo mkdir -p /opt/seafile
 sudo useradd seafile
 sudo chown seafile: /opt/seafile
 wget -qc https://s3.eu-central-1.amazonaws.com/download.seadrive.org/seafile-server_11.0.13_x86-64.tar.gz -O - | tar -xz -C /opt/seafile/
-sudo su - seafile -c "bash /opt/seafile/seafile-server-11.0.13/setup-seafile-mysql.sh"
+#sudo su - seafile -c "bash /opt/seafile/seafile-server-11.0.13/setup-seafile-mysql.sh"
 msg_ok "Installed Seafile"
 
 msg_info "Setting up Memcached"
