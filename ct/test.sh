40a36cb1d3f2a39959be671455740ffbafbec5dc
Dave Yap <github@dave.yap-family.com>
2025-02-25 16:16:13 +0800

    Update test.sh

--- ct/test.sh
+++ ct/test.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 source <(curl -s https://raw.githubusercontent.com/dave-yap/ProxmoxVE/refs/heads/test/misc/build.func)
 # Copyright (c) 2021-2025 tteck
 # Author: dave-yap (dave-yap)
