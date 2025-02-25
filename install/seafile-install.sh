b99add08adf0a89f9207082f050c3a6f99f1dbfe
Dave Yap <github@dave.yap-family.com>
2025-02-26 02:50:50 +0800

    Update seafile-install.sh

--- install/seafile-install.sh
+++ install/seafile-install.sh
@@ -56,7 +56,7 @@ sudo -u mysql mysql -s -e "GRANT ALL PRIVILEGES ON $SEAHUB_DB.* TO '$DB_USER'@lo
 msg_ok "MariaDB setup for Seafile"
 
 msg_info "Installing Seafile Python Dependecies"
-sudo pip3 install \
+pip3 install \
     django==4.2.* \
     future==0.18.* \
     mysqlclient==2.1.* \
@@ -135,7 +135,7 @@ msg_info "Setting up Memcached"
 $STD sudo apt-get install -y \
     memcached \
     libmemcached-dev
-sudo pip3 install \
+pip3 install \
     pylibmc \
     django-pylibmc &>/dev/null
 systemctl enable --now -q memcached
