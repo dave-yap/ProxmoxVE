79a219b374d4c45102345bcd5414d7d64fbdeac1
Dave Yap <github@dave.yap-family.com>
2025-02-27 19:04:23 +0800

    Update mastodon-install.sh

--- install/mastodon-install.sh
+++ install/mastodon-install.sh
@@ -28,6 +28,7 @@ msg_ok "Installed Dependecies"
 msg_info "Installing Node.JS"
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_22.x nodistro main" > /etc/apt/sources.list.d/nodesource.list
+$STD apt-get update -y
 $STD apt-get install -y nodejs
 corepack enable
 msg_ok "Installed Node.JS"
