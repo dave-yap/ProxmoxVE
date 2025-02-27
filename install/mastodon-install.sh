01a2a12410985bd91c879676e7df2831442037bb
Dave Yap <github@dave.yap-family.com>
2025-02-27 19:09:07 +0800

    Update mastodon-install.sh

--- install/mastodon-install.sh
+++ install/mastodon-install.sh
@@ -66,7 +66,7 @@ $STD apt-get install -y \
     libicu-dev \
     libjemalloc-dev
 corepack enable
-useradd mastodon
+adduser --disabled-password mastodon
 mkdir -p /home/mastodon
 mkdir -p /opt/mastodon
 chown mastodon: /home/mastodon
