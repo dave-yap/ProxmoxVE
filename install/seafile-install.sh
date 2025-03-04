9e7fbfdb0bbd571c93d5094f5c974a0e96c40272
Dave Yap <github@dave.yap-family.com>
2025-03-04 11:45:01 +0800

    Update seafile-install.sh

--- install/seafile-install.sh
+++ install/seafile-install.sh
@@ -189,9 +189,9 @@ sed -i "0,/127.0.0.1/s/127.0.0.1/0.0.0.0/" /opt/seafile/conf/gunicorn.conf.py
 msg_ok "Conf files adjusted"
 
 msg_info "Setting up Seafile" 
-$STD su - seafile -c "/opt/seafile/seafile-server-latest/seafile.sh start"
+$STD su - seafile -c "bash /opt/seafile/seafile-server-latest/seafile.sh start"
 $STD su - seafile -c "expect <<EOF
-spawn /opt/seafile/seafile-server-latest/seahub.sh start
+spawn bash /opt/seafile/seafile-server-latest/seahub.sh start
 expect {
     \"email\" {
         send \"admin@localhost.local\r\"
@@ -205,13 +205,12 @@ expect {
 expect {
     \"password again\" {
         send \"helper-scripts\r\"
+        }
     }
-}
 expect eof
 EOF"
-sleep 10
-$STD su - seafile -c "/opt/seafile/seafile-server-latest/seahub.sh stop"
-$STD su - seafile -c "/opt/seafile/seafile-server-latest/seafile.sh stop"
+$STD su - seafile -c "bash /opt/seafile/seafile-server-latest/seahub.sh stop"
+$STD su - seafile -c "bash /opt/seafile/seafile-server-latest/seafile.sh stop"
 msg_ok "Seafile setup"
 
 msg_info "Creating Services"
