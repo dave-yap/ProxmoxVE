2587d52785bc9da974c12c0abeeaa6622fe5deac
Dave Yap <github@dave.yap-family.com>
2025-03-05 00:46:08 +0800

    Update mastodon-install.sh

--- install/mastodon-install.sh
+++ install/mastodon-install.sh
@@ -132,23 +132,22 @@ cd /opt/mastodon && /home/mastodon/.rbenv/shims/bundle config without 'developme
 cd /opt/mastodon && /home/mastodon/.rbenv/shims/bundle install -j$(getconf _NPROCESSORS_ONLN)
 mkdir -p /opt/mastodon/.yarn/patches
 cat > .yarn/patches/babel-plugin-lodash-npm-3.3.4-c7161075b6.patch << 'PATCH'
-diff --git a/lib/index.js b/lib/index.js
-index 3aabfaa..e4a60b5 100644
---- a/lib/index.js
-+++ b/lib/index.js
-@@ -90,6 +90,8 @@ module.exports = function (_ref) {
-                 // The lodash path will be either 'lodash' for ES modules
-                 // or 'lodash/foo' for CommonJS modules.
-                 var lodashPath = path.node.source.value;
-+                if (lodashPath === 'lodash-es')
-+                  lodashPath = 'lodash';
-                 var matches = void 0;
- 
-                 if (lodashPath === 'lodash') {
+diff --git a/package.json b/package.json
+index 9a32427..9a32427 100644
+--- a/package.json
++++ b/package.json
+@@ -1,6 +1,6 @@
+ {
+   "name": "babel-plugin-lodash",
+-  "version": "3.3.4",
++  "version": "3.3.4",
+   "description": "Lodash modularized builds without the hassle",
+   "repository": "lodash/babel-plugin-lodash",
+   "license": "MIT"
 PATCH
 EOF
 #$STD npm install -g yarn@1.22.19 --force
-yarn set version 4.1.1
+yes | yarn set version 4.1.1
 $STD su - mastodon -c "cd /opt/mastodon && yes | yarn install"
 $STD su - mastodon -c "cd /opt/mastodon && . ~/.bashrc && expect <<EOF
 spawn env RAILS_ENV=production /opt/mastodon/bin/rails mastodon:setup
